c7a9cb133789536c72e61006f76f591d5d49a8d2
Alec Robitaille <robit.alec@gmail.com>
2018-03-27 16:44:23 -0230

    generate grid as raster

--- R/functions/GenerateGrid.R
+++ R/functions/GenerateGrid.R
@@ -1,18 +1,38 @@
-GenerateGrid <- function(spacing, mcpExtent, DT = NULL, xCol = NULL, yCol = NULL){
+GenerateGrid <- function(spacing, crs, mcpExtent, DT = NULL, xCol = NULL, yCol = NULL){
   
   extentDT <- data.table(wolfMCP@bbox, keep.rownames = TRUE)
   
   extentDT[, dif := abs(min - max)]  
   
+  
   if(extentDT[order(-dif)][1, rn] == "x"){
     xSeq <- extentDT[rn == "x", seq(min, max, by = spacing)]
     ySeq <- extentDT[rn == "y", seq(min, min + (spacing * (length(xSeq) - 1)),
                                     by = spacing)]
-    data.table(x = xSeq, y = ySeq, rowID = 1:length(xSeq))
   } else {
     ySeq <- extentDT[rn == "y", seq(min, max, by = spacing)]
     xSeq <- extentDT[rn == "x", seq(min, min + (spacing * (length(ySeq) - 1)),
                                     by = spacing)]
-    data.table(x = xSeq, y = sySeq, rowID = 1:length(xSeq))
   }
+
+  r <- raster::extent(head(xSeq, n = 1), tail(xSeq, n = 1), 
+                      head(ySeq, n = 1), tail(ySeq, n = 1))
+  ra <- raster(r)
+  res(ra) <- c(spacing, spacing)
+  
+  projection(ra) <- CRS(crs)
+  
+  ra
+  
+  # if(extentDT[order(-dif)][1, rn] == "x"){
+  #   xSeq <- extentDT[rn == "x", seq(min, max, by = spacing)]
+  #   ySeq <- extentDT[rn == "y", seq(min, min + (spacing * (length(xSeq) - 1)),
+  #                                   by = spacing)]
+  #   data.table(x = xSeq, y = ySeq, rowID = 1:length(xSeq))
+  # } else {
+  #   ySeq <- extentDT[rn == "y", seq(min, max, by = spacing)]
+  #   xSeq <- extentDT[rn == "x", seq(min, min + (spacing * (length(ySeq) - 1)),
+  #                                   by = spacing)]
+  #   data.table(x = xSeq, y = sySeq, rowID = 1:length(xSeq))
+  # }
 }
