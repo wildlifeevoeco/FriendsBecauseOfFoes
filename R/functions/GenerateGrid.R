06e6c914570107dec6c36f285277b3e76e7000c0
Alec Robitaille <robit.alec@gmail.com>
2018-03-27 16:49:24 -0230

    return sp from grid

--- R/functions/GenerateGrid.R
+++ R/functions/GenerateGrid.R
@@ -22,17 +22,6 @@ GenerateGrid <- function(spacing, crs, mcpExtent, DT = NULL, xCol = NULL, yCol =
   
   projection(ra) <- CRS(crs)
   
-  ra
+  rasterToPoints(ra, spatial = TRUE)
   
-  # if(extentDT[order(-dif)][1, rn] == "x"){
-  #   xSeq <- extentDT[rn == "x", seq(min, max, by = spacing)]
-  #   ySeq <- extentDT[rn == "y", seq(min, min + (spacing * (length(xSeq) - 1)),
-  #                                   by = spacing)]
-  #   data.table(x = xSeq, y = ySeq, rowID = 1:length(xSeq))
-  # } else {
-  #   ySeq <- extentDT[rn == "y", seq(min, max, by = spacing)]
-  #   xSeq <- extentDT[rn == "x", seq(min, min + (spacing * (length(ySeq) - 1)),
-  #                                   by = spacing)]
-  #   data.table(x = xSeq, y = sySeq, rowID = 1:length(xSeq))
-  # }
 }
