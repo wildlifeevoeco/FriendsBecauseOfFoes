d76ddac77d4008a169edd4c582af6ff207a9fc8f
Alec Robitaille <robit.alec@gmail.com>
2018-03-26 22:05:23 -0230

    store consistent output fields

---
+++ R/variables/PrepDataOutputVariables.R
@@ -0,0 +1,4 @@
+outputVariables <- c('id', 'species', 'herd', 'sex', 
+                    'idate', 'itime', 'datetime', 
+                    'EASTING', 'NORTHING',
+                    'julday', 'yr', 'mnth', 'stepLength', 'moveRate', 'fixRate')
