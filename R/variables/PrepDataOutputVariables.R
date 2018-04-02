e9a40032d2da5116bf1c2b4194f38cedaa548edc
Alec Robitaille <robit.alec@gmail.com>
2018-04-02 12:37:35 -0230

    fix output variables

--- R/variables/PrepDataOutputVariables.R
+++ R/variables/PrepDataOutputVariables.R
@@ -1,4 +1,4 @@
-outputVariables <- c('id', 'species',
+outputVariables <- c('id', 'species', 'season',
                     'idate', 'itime', 'datetime', 
                     'EASTING', 'NORTHING',
                     'julday', 'yr', 'mnth', 'stepLength', 'moveRate', 'fixRate')
