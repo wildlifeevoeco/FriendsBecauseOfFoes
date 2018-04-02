6a243f718b5800f6ef59cedd289e57e1dd5a0f1e
Alec Robitaille <robit.alec@gmail.com>
2018-04-02 17:10:46 -0230

    elk used and available sampling

--- R/method-devel/prey-rsf/ElkRSF.R
+++ R/method-devel/prey-rsf/ElkRSF.R
@@ -26,8 +26,9 @@ bounds <- rgdal::readOGR('input/etc/RMNP-extent/RMNPextent.shp') %>%
   spTransform(CRSobj = utm)
 
 # Covariates
-lsCovers <- data.table(nm = dir('input/covariates/RMNP', '.tif$'))[, nm := gsub(".tif|100m", "", nm)]
-lsPaths <- data.table(nm = dir('input/covariates/RMNP', '.tif$', full.names = TRUE))
+lsCovers <- data.table(nm = dir('input/covariates/RMNP', '.tif$'))[, 
+  nm := gsub(".tif|100m", "", nm)]$nm
+lsPaths <- dir('input/covariates/RMNP', '.tif$', full.names = TRUE)
 
 ### Processing ----
 # MCPs
@@ -38,10 +39,10 @@ elkMCP <- mcp(elkSP, 100)
 
 # Create Regular Grid
 source('R/functions/GenerateGrid.R')
-regGrid <- GenerateGrid(3000, mcpExtent = elkMCP, crs = utm)
+regPts <- GenerateGrid(3000, mcpExtent = elkMCP, crs = utm)
 
-regPts <- data.table(regGrid@coords)[over(elkMCP, regGrid, returnList = TRUE)[[1]]]
 setnames(regPts, c('EASTING', 'NORTHING'))
+
 
 # Check that points are within MCP
 ggplot(elkMCP) +
@@ -55,61 +56,27 @@ elk[, observed := 1]
 samplePts <- rbindlist(list(regPts, elk), 
                        use.names = TRUE, fill = TRUE)
 
-### RSF ----
+### Sampling ----
 # Drop columns leaving only needed
-cols <- c('id','EASTING', 'NORTHING','season')
-
+cols <- c('id','EASTING', 'NORTHING', 'season', 'observed')
 samplePts <- samplePts[, ..cols]
 
-
-# These should also match (they do)
-nrow(available.elk)
-nrow(observed.elk)
-
-# Combine the observed and random elk and assign a rowID
-sample.elk <- rbindlist(list(available.elk, observed.elk))
-
 # Add row ID
-sample.elk[, rowID := .I]
+samplePts[, rowID := .I]
 
-saveRDS(sample.elk, 'output/sample-elk')
-sample.elk <- readRDS('output/sample-elk')
+# Sample rasters
+samplePts[, (lsCovers) := lapply(lsPaths, FUN = function(r){
+  extract(raster(r), matrix(c(EASTING, NORTHING), ncol = 2))})]
 
-
-
-####extract covariates for used and available
-
-### Extract land cover, DEM and focal rasters ----
-## Functions
-# Extract raster values at points
-ExtractPoints <- function(pt.matrix, raster.layer){
-  # Extract in raster layer values at points
-  extract(raster.layer, pt.matrix)
-}
-  
-##Covariate sampling
-# Sample landcover at each point
-sample.elk[, agprop := ExtractPoints(matrix(c(EASTING, NORTHING), ncol = 2), agriculture)]
-sample.elk[, bgprop := ExtractPoints(matrix(c(EASTING, NORTHING), ncol = 2), bog)]
-sample.elk[, cnprop := ExtractPoints(matrix(c(EASTING, NORTHING), ncol = 2), coniferous)]
-sample.elk[, dcprop := ExtractPoints(matrix(c(EASTING, NORTHING), ncol = 2), deciduous)]
-sample.elk[, grprop := ExtractPoints(matrix(c(EASTING, NORTHING), ncol = 2), grassland)]
-sample.elk[, hudist := ExtractPoints(matrix(c(EASTING, NORTHING), ncol = 2), human)]
-sample.elk[, mrprop := ExtractPoints(matrix(c(EASTING, NORTHING), ncol = 2), marsh)]
-sample.elk[, mwprop := ExtractPoints(matrix(c(EASTING, NORTHING), ncol = 2), mixedwood)]
-sample.elk[, odprop := ExtractPoints(matrix(c(EASTING, NORTHING), ncol = 2), opendec)]
-sample.elk[, rgdns := ExtractPoints(matrix(c(EASTING, NORTHING), ncol = 2), ruggedness)]
-sample.elk[, wtdist := ExtractPoints(matrix(c(EASTING, NORTHING), ncol = 2), water)]
-
-
-###winter RSF
-winter.elk <- sample.elk[season=="winter"]
+### RSF ====
+# Winter RSF
+winterElk <- samplePts[season == "winter"]
 
 winterelkrsf <- glm(observed~agprop+bgprop+cnprop+dcprop+grprop+hudist+mrprop+mwprop+odprop+rgdns+wtdist, family = binomial,data=winter.elk)
 
 
-###spring RSF
-spring.elk <- sample.elk[season=="spring"]
+# Spring RSF
+springElk <- samplePts[season == "spring"]
 
 springelkrsf <- glm(observed~agprop+bgprop+cnprop+dcprop+grprop+hudist+mrprop+mwprop+odprop+rgdns+wtdist, family = binomial,data=spring.elk)
 
