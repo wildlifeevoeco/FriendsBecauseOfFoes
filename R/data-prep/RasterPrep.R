dd2bbe74a44a392ac183d134db2ab3cc2d02ba49
Christina Prokopenko <cmprokopenko@mun.ca>
2018-04-03 11:55:15 -0230

    log transform

--- R/data-prep/RasterPrep.R
+++ R/data-prep/RasterPrep.R
@@ -9,8 +9,7 @@
 
 ### Packages ----
 libs <- c('data.table', 'ggplot2',
-          'sp', 'raster',
-          'magrittr')
+          'sp', 'magrittr', 'raster')
 lapply(libs, require, character.only = TRUE)
 
 ### Input data ----
@@ -25,12 +24,37 @@ bounds <- rgdal::readOGR('input/etc/RMNP-extent/RMNPextent.shp') %>%
 lsCovers <- data.table(nm = dir('input/covariates/RMNP', '.tif$'))[, nm := gsub(".tif|100m", "", nm)]$nm
 lsPaths <- dir('input/covariates/RMNP', '.tif$', full.names = TRUE)
 
+### Processing ----
 # Crop the rasters, holding as temp files in a list
 cropRasters <- lapply(lsPaths, FUN = function(r){
   crop(raster(r), bounds)
 })
 
+# Log transform
+namesTransform <- c('LinFeat_Dist', 'Water_Dist')
+rasterTransform <- cropRasters[lapply(cropRasters, names) %in% namesTransform]
 
+transformed <- lapply(seq_along(namesTransform), FUN = function(x){
+   r <- log(rasterTransform[[x]] + 1)
+   names(r) <- namesTransform[[x]]
+   r
+})
+
+outRasters <- c(transformed, cropRasters[!(lapply(cropRasters, names) %in% namesTransform)])
+
+
+
+### Output ----
+ls.rsf <- list('WINTERELK' = winterElkRSF.rstr, 
+               'SPRINGELK' = springElkRSF.rstr)
+
+namecropRasters
+
+lapply(seq_along(ls.rsf), FUN = function(r){
+  writeRaster(ls.rsf[[r]], paste0('output/prey-rsf/elkrsf', names(ls.rsf[r])), 
+              format = 'raster',
+              overwrite = T)
+})
 
 
 
