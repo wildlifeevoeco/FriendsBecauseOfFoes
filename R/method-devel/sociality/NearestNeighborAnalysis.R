e094995b2323ff04122eb12cad73dc96a30c7b4b
Alec Robitaille <robit.alec@gmail.com>
2018-04-03 10:53:56 -0230

    fst nearest min function

--- R/method-devel/sociality/NearestNeighborAnalysis.R
+++ R/method-devel/sociality/NearestNeighborAnalysis.R
@@ -17,6 +17,10 @@ lapply(libs, require, character.only = TRUE)
 ### Input data ----
 elk <- readRDS('output/data-prep/elk.Rds')
 
+coordCols <- c('EASTING', 'NORTHING')
+
+elk <- elk[julday == 25 & id < 300][, nbyid := .N, by = id][nbyid == 1]
+
 ### Dist ----
 # Apply dist by time step
 MeanPairwiseDists <- function(DT, coordCols, idCol){
@@ -29,16 +33,52 @@ MeanPairwiseDists <- function(DT, coordCols, idCol){
   dists#[, lapply(.SD, min), by = rn]
 }
 
-elk[, MeanPairwiseDists(.SD, coordCols, 'id'), by = julday]
+ggplot(aes(EASTING, NORTHING, color = factor(id)), 
+       data = elk) +
+  geom_point() + ggthemes::scale_colour_pander() +
+  coord_fixed()
 
-data.matrix(dist(elk[julday == 200, ..coordCols],
-     method = "euclidean",
-     diag = FALSE,
-     upper = TRUE), keep.rownames = TRUE)
+
+# elk[, MeanPairwiseDists(.SD, coordCols, 'id'), by = julday]
+
+
+apply(data.table(a), 2, which.min)
+
+
+
+data.table(a)[, apply(.SD, 1, which.min)]
+
+data.table(a.)[, [which.min(.SD)]]
+
+Nfunction = function(DT){
+  a <- data.matrix(dist(DT[, ..coordCols],
+                        method = "euclidean",
+                        diag = FALSE,
+                        upper = TRUE))
+  diag(a) <- NA
+  
+  lsids <- DT$id
+  
+  dtA <- data.table(a, id = lsids)
+  setnames(dtA, c(lsids, 'id'))
+  # dtA[, id[which.min(.SD)]]
+  b <- dtA[, lapply(.SD, FUN = function(x) id[which.min(x)]),
+      .SDcols = as.character(lsids)]
+  suppressWarnings(melt(b, variable.name = 'left', value.name = 'right'))
+
+}
+
+
+Nfunction(elk)
+
+
+system.time(
+  data.table(a, keep.rownames = TRUE)[, rn[which.min(`2`)]]
+)
+
 
 ### Quadtree ----
 elk[, rowID := .I]
-coordCols <- c('EASTING', 'NORTHING')
 NumberQuadtreeNeighbors <- function(DT, coordCols, numbNeighbors) {
   tree <- createTree(DT)
   knnLookup(tree, newdat = DT, k = numbNeighbors)[, 2]
