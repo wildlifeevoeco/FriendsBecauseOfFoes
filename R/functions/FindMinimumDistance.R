5b2ed808c5b920d0a984f35e3420854d73c75e40
Alec Robitaille <robit.alec@gmail.com>
2018-04-03 13:28:02 -0230

    fst separate min dist function

---
+++ R/functions/FindMinimumDistance.R
@@ -0,0 +1,17 @@
+FindMinimumDistance = function(DT, coordCols, idCol){
+  a <- as.matrix(dist(DT[, ..coordCols],
+                      method = 'euclidean'))
+  diag(a) <- NA
+  
+  lsID <- DT[, get(idCol)]
+  
+  dtA <- data.table(a, id = lsID)
+  
+  setnames(dtA, c(lsID, 'id'))
+  
+  findMin <- dtA[, lapply(.SD, FUN = function(x) id[which.min(x)]),
+                 .SDcols = as.character(lsID)]
+  
+  suppressWarnings(melt(findMin, variable.name = 'left',
+                        value.name = 'right'))
+}
