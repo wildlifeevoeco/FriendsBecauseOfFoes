cb1a88b54f20a317b928385acea295aec539df81
Christina Prokopenko <cmprokopenko@mun.ca>
2018-04-03 14:35:55 -0230

    better ls indexing for remove irrelevant rsf cov

--- R/method-devel/prey-rsf/ElkRSF.R
+++ R/method-devel/prey-rsf/ElkRSF.R
@@ -27,8 +27,8 @@ bounds <- rgdal::readOGR('input/etc/RMNP-extent/RMNPextent.shp') %>%
 
 # Covariates
 lsCovers <- data.table(nm = dir('output/data-prep/cropped-rasters/RMNP', '.tif$'))[, 
-  nm := gsub(".tif|100m", "", nm)]$nm[-1][-3]
-lsPaths <- dir('output/data-prep/cropped-rasters/RMNP', '.tif$', full.names = TRUE)[-1][-3]
+  nm := gsub(".tif|100m", "", nm)]$nm[-c(1, 4, 5)]
+lsPaths <- dir('output/data-prep/cropped-rasters/RMNP', '.tif$', full.names = TRUE)[-c(1, 4, 5)]
 
 ### Processing ----
 # MCPs
@@ -44,7 +44,7 @@ regPts <- GenerateGrid(90, mcpExtent = elkMCP, crs = utm)
 setnames(regPts, c('EASTING', 'NORTHING'))
 
 # saveRDS(regPts, 'output/prey-rsf/elkRegularPoints.Rds')
-regPts <- readRDS('output/prey-rsf/elkRegularPoints.Rds')
+#regPts <- readRDS('output/prey-rsf/elkRegularPoints.Rds')
 
 # Check that points are within MCP
 # ggplot(elkMCP) +
@@ -71,7 +71,7 @@ samplePts[, (lsCovers) := lapply(lsPaths, FUN = function(r){
   extract(raster(r), matrix(c(EASTING, NORTHING), ncol = 2))})]
 
 # saveRDS(samplePts, 'output/prey-rsf/elkSamplePoints.Rds')
-samplePts <- readRDS('output/prey-rsf/elkSamplePoints.Rds')
+#samplePts <- readRDS('output/prey-rsf/elkSamplePoints.Rds')
 
 ### RSF ====
 lsRasters <- lapply(lsPaths, raster)
@@ -96,8 +96,8 @@ winElk.b <- coef(winterElkRSF)[-1]
 
 
 # Create the raster matching the first raster layer with the first fixed effect
-winterElkRSF.rstr <- (lsRasters[[1]] * winElk.b[1] + lsRasters[[2]] * winElk.b[2] +
-                        lsRasters[[3]] * winElk.b[3] + resample(lsRasters[[4]], lsRasters[[3]]) * winElk.b[4] + 
+winterElkRSF.rstr <- exp(lsRasters[[1]] * winElk.b[1] + lsRasters[[2]] * winElk.b[2] +
+                        lsRasters[[3]] * winElk.b[3] + lsRasters[[4]] * winElk.b[4] + 
                         lsRasters[[5]] * winElk.b[5]+ lsRasters[[6]] * winElk.b[6]+ 
                         lsRasters[[7]] * winElk.b[7]+ #lsRasters[[8]] * winElk.b[8]+ 
                         lsRasters[[9]] * winElk.b[9])
@@ -109,17 +109,30 @@ plot(winterElkRSF.rstr)
 springElk <- samplePts[season == "spring" | is.na(season)]
 springElk[observed == 0, season := "spring"]
 
-springElkRSF <- glm(observed ~ Bog + Coniferous + Grassland + log(LinFeat_Dist+1) + 
-                      Marsh + Mixedwood + Opendeciduous + Ruggedness_test  + log(Water_Dist+1), 
-                    family = binomial,
-                    data = springElk)
+#springElkRSF <- glm(observed ~ Bog + Coniferous + Grassland + log(LinFeat_Dist+1) + 
+#                     Marsh + Mixedwood + Opendeciduous + Ruggedness_test  + log(Water_Dist+1), 
+#                    family = binomial,
+#                    data = springElk)
 
-springElkRSF <- glm(reformulate(lsCovers, response = 'observed'),  #### Dist not logged yet
+springElkRSF <- glm(reformulate(lsCovers, response = 'observed'), 
        family = 'binomial',data = springElk)
 
 summary(springElkRSF)
-vif(springElkRSF)
 rsquared(springElkRSF)
+
+# Pull out the coefficients, dropping the intercept
+sprElk.b <- coef(springElkRSF)[-1]
+
+
+# Create the raster matching the first raster layer with the first fixed effect
+springElkRSF.rstr <- (lsRasters[[1]] * sprElk.b[1] + lsRasters[[2]] * sprElk.b[2] +
+                        lsRasters[[3]] * sprElk.b[3] + lsRasters[[4]] * sprElk.b[4] + 
+                        lsRasters[[5]] * sprElk.b[5]+ lsRasters[[6]] * sprElk.b[6]+ 
+                        lsRasters[[7]] * sprElk.b[7]+ #lsRasters[[8]] * sprElk.b[8]+ 
+                        lsRasters[[9]] * sprElk.b[9])
+
+plot(springElkRSF.rstr)
+
 
 ### Save the RSFs ----
 ls.rsf <- list('WINTERELK' = winterElkRSF.rstr, 
