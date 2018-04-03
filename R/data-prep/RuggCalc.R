618dc2e49a0c05ed18515290d42004482ad1d67e
Christina Prokopenko <cmprokopenko@mun.ca>
2018-04-03 14:36:01 -0230

    fst rugged calc

---
+++ R/data-prep/RuggCalc.R
@@ -0,0 +1,13 @@
+
+
+
+elevation <- raster('input/covariates/RMNP/Elevation.tif')
+
+#####calculate ruggedness from elevation
+
+ruggedness<- terrain(elevation,opt="roughness")
+writeRaster(ruggedness,'input/covariates/RMNP/Ruggedness', 
+              format = 'GTiff', 
+              overwrite = T)
+
+plot(ruggedness)
