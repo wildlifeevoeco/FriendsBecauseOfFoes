2a84678c8baa8d1b4ecb614a7576fc9b76b0a060
Alec Robitaille <robit.alec@gmail.com>
2018-04-18 14:36:07 -0230

    coords/coordsCol strange data.table error

--- R/functions/NumbQuadTreeNeighbours.R
+++ R/functions/NumbQuadTreeNeighbours.R
@@ -1,6 +1,6 @@
-NumbQuadTreeNeighbours <- function(DT, coordCols, numbNeighbours, idCol) {
-  tree <- createTree(DT[, ..coordCols])
-  knn <- DT[, get(idCol)[knnLookup(tree, newdat = DT[, ..coordCols], 
+NumbQuadTreeNeighbours <- function(DT, coords, numbNeighbours, idCol) {
+  tree <- createTree(DT[, ..coords])
+  knn <- DT[, get(idCol)[knnLookup(tree, newdat = DT[, ..coords], 
                                    k = numbNeighbours + 1)[, -1]]]
   dim(knn) <- c(length(DT[, get(idCol)]), numbNeighbours)
   as.data.table(knn)
