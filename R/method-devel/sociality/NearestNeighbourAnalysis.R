c2431c5f0f4a4bbcaa8fa4cb96e6fcc38422742c
Alec Robitaille <robit.alec@gmail.com>
2018-04-18 12:16:27 -0230

    merge quadtree NN #8

--- R/method-devel/sociality/NearestNeighbourAnalysis.R
+++ R/method-devel/sociality/NearestNeighbourAnalysis.R
@@ -1,4 +1,4 @@
-### Nearest Neighbor Analysis ----
+### Nearest Neighbour Analysis ----
 # Authors: Alec Robitaille
 # Purpose: 
 # Inputs: Elk, caribou relocation data
@@ -20,7 +20,8 @@ elk <- readRDS('output/data-prep/elk.Rds')
 coordCols <- c('EASTING', 'NORTHING')
 idCol <- 'id'
 
-### Find Minimum Distance Neighbor ----
+
+### Checks ----
 # Do any timegroups have the same individual twice?
 all.equal(elk[, .(N = uniqueN(id)), by = timegroup],
           elk[, .N, by = timegroup])
@@ -29,60 +30,56 @@ all.equal(elk[, .(N = uniqueN(id)), by = timegroup],
 elk[, NbyTime := .N, by = timegroup]
 elk[, qplot(NbyTime)]
 
-# Calc Nearest Neighbour
-source('R/functions/FindMinimumDistance.R')
-nn <- elk[NbyTime > 1, FindMinimumDistance(.SD, coordCols, idCol),
-    by = timegroup]
-
-# Merge the nearest neighbors back on
-withNeighbours <- merge(elk, nn, 
-                        by.x = c('id', 'timegroup'),
-                        by.y = c('left', 'timegroup'))
-
-# And row bind those in a timegroup alone
-elk <- rbindlist(list(withNeighbours, elk[NbyTime == 1]), fill = TRUE)
-
-
-### Quadtree ----
-# How many neighbors 
-neighbours <- 3
+### Quadtree - Find Nearest Neighbour ----
+# How many neighbours 
+neighbours <- 1
 neighbourCols <- paste0('neighbour', seq(1, neighbours))
 
+# Read in function
 source('R/functions/NumbQuadTreeNeighbours.R')
-
-# Only running on where there are at least 2 in a timegroup, else ! the bomb !
+# Only running on where there are at least 2 in a timegroup, else the bomb!
 elk[NbyTime > neighbours, 
     (neighbourCols) := NumbQuadTreeNeighbours(.SD, coordCols,
                                               neighbours, idCol),
     by = timegroup]
 
-elk
-elk[timegroup == 5, .(id, timegroup, neighbour1, neighbour2, neighbour3)]
-elk[, c(neighbourCols, 'timegroup'), with = FALSE]
-elk[, !c(neighbourCols), with = FALSE]
 
-mergeElk <- merge(elk[, c(neighbourCols, 'timegroup'), with = FALSE], 
-                  elk[, !c(neighbourCols), with = FALSE],
-                  by.x = c('neighbour1', 'timegroup'),
-                  by.y = c('id', 'timegroup'),
-                  suffixes = c("Right", ""), all.y = TRUE)
+# NA in neighbour means that there were less than the NbyTime in the timegroup
+elkNN <- merge(elk[, .(id, EASTING, NORTHING, neighbour1, timegroup)],
+               elk[, .(neighbour1 = id, rEASTING = EASTING, rNORTHING = NORTHING, timegroup)],
+               all.x = TRUE)
+
 
 ### Calculate dyadic distance ----
 source('R/functions/DyadicDistance.R')
 # dyadDistCols <- paste0('dyadDist', seq(1, neighbours))
 
-# TODO flex for multiple neighbors
+# TODO flex for multiple neighbours
 # elk[, (dyadDistCols) := DyadicDistance(.SD, coordCols = coordCols,
 #                                        neighbourCoordCols = paste0(coordCols, "Right"))]
 
-DyadicDistance(withRight, coordCols = coordCols,
-               neighbourCoordCols = paste0(coordCols, "Right"))
+DyadicDistance(elkNN, coordCols = coordCols,
+               neighbourCoordCols = paste0("r", coordCols))
 
 ### Compare methods ----
 elk[!is.na(right)]
 elk[!is.na(neighbour1)]
 elk[neighbour1 == right]
 elk[neighbour1 != right]
+
+### Find Minimum Distance Neighbour ----
+# Calc Nearest Neighbour
+source('R/functions/FindMinimumDistance.R')
+nn <- elk[NbyTime > 1, FindMinimumDistance(.SD, coordCols, idCol),
+          by = timegroup]
+
+# Merge the nearest neighbours back on
+withNeighbours <- merge(elk, nn, 
+                        by.x = c('id', 'timegroup'),
+                        by.y = c('left', 'timegroup'))
+
+# And row bind those in a timegroup alone
+elk <- rbindlist(list(withNeighbours, elk[NbyTime == 1]), fill = TRUE)
 
 
 ### Figures ----
