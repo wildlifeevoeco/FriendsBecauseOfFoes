398d07362813d1e2936f6106d8a79dbfb58446dd
Alec Robitaille <robit.alec@gmail.com>
2018-04-18 16:24:33 -0230

    description abs TA

--- R/method-devel/sociality/AbsoluteTurnAngle.R
+++ R/method-devel/sociality/AbsoluteTurnAngle.R
@@ -1,31 +1,49 @@
-### Absolute Turn Angle
-# Alec Robitaille
-# Started March 15 2018
+### Absolute Turn Angle ----
+# Authors: Alec Robitaille
+# Started: March 15 2018
+# Purpose: Calculate absolute turn angle between locs of an individual
+# Inputs: Elk, caribou relocation data
+# Outputs: Abs TA data
+# Project: Easter Week Challenge 2018
+# Copyright: ./LICENSE.md 
 
-# For calculation of absolute turn angle between 
-# point at time and time+1 
 
-
+### Theory ====
 # Math:
 # arctan(dif-y, dif-x) * 180 / pi
 
+### Packages ----
+libs <- c('data.table', 'ggplot2',
+          'SearchTrees',
+          'magrittr')
+lapply(libs, require, character.only = TRUE)
 
-DT <- readRDS('output/data-prep/caribou.Rds')[1:100]
-DT[, rowID := rleid(EASTING)]
-DT[1:5, .(rowID, EASTING, NORTHING, lagEASTING, lagNORTHING, difX, difY, stepLength)]
-qplot(EASTING, NORTHING, color = absAngle, data = DT)
+
+### Input data ----
+elk <- readRDS('output/data-prep/elk.Rds')
+
+coordCols <- c('EASTING', 'NORTHING')
+idCol <- 'id'
+
+
+### Variables ----
+elk[, rowID := rleid(EASTING)]
+
+qplot(EASTING, NORTHING, color = absAngle, data = elk)
 
 source('R/functions/StepLength.R')
-StepLength(DT, 'id', datetimeCol = 'datetime', yrCol = 'yr',
-           xCol = projXCol, yCol = projYCol,
+StepLength(elk, 'id', datetimeCol = 'datetime', yrCol = 'yr',
+           xCol = coordCols[1], yCol = coordCols[2],
            returnIntermediate = TRUE)
 
-DT[, absAngle := atan2(difY, difX) * 180 / pi]
-DT[absAngle < 0, absAngle := absAngle + 360]
-ggplot(DT, aes(EASTING, NORTHING)) + 
+elk[, absAngle := atan2(difY, difX) * 180 / pi]
+elk[absAngle < 0, absAngle := absAngle + 360]
+
+ggplot(elk, aes(EASTING, NORTHING)) + 
   geom_path(aes(color = absAngle)) #+ 
   # geom_path()
-DT[, hist(absAngle)]
+elk[, hist(absAngle)]
+
 # Pseudo code
 
 # Shift points 1 row
