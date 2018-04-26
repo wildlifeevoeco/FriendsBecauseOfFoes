e2a3c6f1531b52f4676f55bf6330de143e833d82
Hance <e.hance.ellington@gmail.com>
2018-04-26 10:49:36 -0230

    updated model code

--- R/5-model/socialitydomainGLM.R
+++ R/5-model/socialitydomainGLM.R
@@ -14,7 +14,25 @@ lapply(libs, require, character.only = TRUE)
 ### Input data ----
 
 #RMNP file
-rmnp <- readRDS('output/nna/elkNNA.Rds')
+
+setwd("C:/Users/ehanc/Desktop/GitLab/ewc/output/nna")
+
+rmnp <- readRDS('elkNNA.Rds')
+
+### creating average RSF values for each dyad
+### Alec you might want to put this in your data prep code
+
+rmnp$avg.PredRSF<-rowMeans(matrix(c(rmnp$preyRSF,rmnp$rpreyRSF),ncol=2))
+rmnp$avg.PreyRSF<-rowMeans(matrix(c(rmnp$preyRSF,rmnp$rpreyRSF),ncol=2))
+
+### one case where dyad is just i-i, instead of i-j
+### this needs to be addressed, why did this occur
+
+rmnp$bad.neighbor<-ifelse(rmnp$id==rmnp$neighbour1,1,0)
+
+### removing this case for now, plus just extracting dyads
+
+rmnp.dyad1<-subset(rmnp, rmnp$bad.neighbor==0)
 
 #NL file
 # nl <- 
@@ -30,23 +48,163 @@ ggplot(dd) +
 unique(rmnp[dyadDist < 150, .(dyadID, timegroup)])
 
 
-rmnp[dyadDist < 500, qplot()
+#rmnp[dyadDist < 500, qplot()
 
      
-     ###GLM
-#nearest neighbour 
-# rmNN <- glm(TurnAngle ~ PreyHD + PredHD + PreyHD:PredHD, data = rmnp)
 ###GLM
- 
-# SA <- glm(SocialMeasure ~ PreyHD + PredHD + PreyHD:PredHD, data = rmnp)
-## social measures are: dSI, nWithin5000,
+# SA <- glm(SocialMeasure ~ PreyHD*PredHD, data = rmnp)
+## social measures are: dSI, dyadDist, dAbsAng, nWithin5000
 
-rmNNWinter <-
-  glm(absAngle ~ elkwinter + wolfwinter + elkwinter:wolfwinter,
-      data = rmnp[season == 'winter'])
+# round(cor(rmnp.dyad1[,c(17,18,26,27,14,25,32,31,34,35,37,38)]),2)
+
+rmNN.dsl.avg.d500<-
+  glm(dSI ~ avg.PreyRSF*avg.PredRSF,
+      data = rmnp.dyad1[dyadDist < 500])
+
+rmNN.dsl.id1.d500<-
+  glm(dSI ~ preyRSF*predatorRSF,
+      data = rmnp.dyad1[dyadDist < 500])
+
+rmNN.dsl.id2.d500<-
+  glm(dSI ~ rpreyRSF*rpredatorRSF,
+      data = rmnp.dyad1[dyadDist < 500])
+
+summary(rmNN.dsl.avg.d500)
+summary(rmNN.dsl.id1.d500)
+summary(rmNN.dsl.id2.d500)
+
+rmNN.dsl.avg.d50<-
+  glm(dSI ~ avg.PreyRSF*avg.PredRSF,
+      data = rmnp.dyad1[dyadDist < 50])
+
+rmNN.dsl.id1.d50<-
+  glm(dSI ~ preyRSF*predatorRSF,
+      data = rmnp.dyad1[dyadDist < 50])
+
+rmNN.dsl.id2.d50<-
+  glm(dSI ~ rpreyRSF*rpredatorRSF,
+      data = rmnp.dyad1[dyadDist < 50])
+
+summary(rmNN.dsl.avg.d50)
+summary(rmNN.dsl.id1.d50)
+summary(rmNN.dsl.id2.d50)
+
+rmNN.dsl.avg.d100<-
+  glm(dSI ~ avg.PreyRSF*avg.PredRSF,
+      data = rmnp.dyad1[dyadDist < 100])
+
+rmNN.dsl.id1.d100<-
+  glm(dSI ~ preyRSF*predatorRSF,
+      data = rmnp.dyad1[dyadDist < 100])
+
+rmNN.dsl.id2.d100<-
+  glm(dSI ~ rpreyRSF*rpredatorRSF,
+      data = rmnp.dyad1[dyadDist < 100])
+
+summary(rmNN.dsl.avg.d100)
+summary(rmNN.dsl.id1.d100)
+summary(rmNN.dsl.id2.d100)
+
+### interesting results disappear at 150m
+
+rmNN.dyadDist.avg.d500<-
+  glm(dyadDist ~ avg.PreyRSF*avg.PredRSF,
+      data = rmnp.dyad1[dyadDist < 500])
+
+rmNN.dyadDist.id1.d500<-
+  glm(dyadDist ~ preyRSF*predatorRSF,
+      data = rmnp.dyad1[dyadDist < 500])
+
+rmNN.dyadDist.id2.d500<-
+  glm(dyadDist ~ rpreyRSF*rpredatorRSF,
+      data = rmnp.dyad1[dyadDist < 500])
+
+summary(rmNN.dyadDist.avg.d500)
+summary(rmNN.dyadDist.id1.d500)
+summary(rmNN.dyadDist.id2.d500)
+
+rmNN.dyadDist.avg.d500to1000<-
+  glm(dyadDist ~ avg.PreyRSF*avg.PredRSF,
+      data = rmnp.dyad1[dyadDist > 500 & dyadDist < 1000])
+
+rmNN.dyadDist.id1.d500to1000<-
+  glm(dyadDist ~ preyRSF*predatorRSF,
+      data = rmnp.dyad1[dyadDist > 500 & dyadDist < 1000])
+
+rmNN.dyadDist.id2.d500to1000<-
+  glm(dyadDist ~ rpreyRSF*rpredatorRSF,
+      data = rmnp.dyad1[dyadDist > 500 & dyadDist < 1000])
+
+summary(rmNN.dyadDist.avg.d500to1000)
+summary(rmNN.dyadDist.id1.d500to1000)
+summary(rmNN.dyadDist.id2.d500to1000)
+
+rmNN.dyadDist.avg.d50<-
+  glm(dyadDist ~ avg.PreyRSF*avg.PredRSF,
+      data = rmnp.dyad1[dyadDist < 50])
+
+rmNN.dyadDist.id1.d50<-
+  glm(dyadDist ~ preyRSF*predatorRSF,
+      data = rmnp.dyad1[dyadDist < 50])
+
+rmNN.dyadDist.id2.d50<-
+  glm(dyadDist ~ rpreyRSF*rpredatorRSF,
+      data = rmnp.dyad1[dyadDist < 50])
+
+summary(rmNN.dyadDist.avg.d50)
+summary(rmNN.dyadDist.id1.d50)
+summary(rmNN.dyadDist.id2.d50)
+
+rmNN.dyadDist.avg.d250<-
+  glm(dyadDist ~ avg.PreyRSF*avg.PredRSF,
+      data = rmnp.dyad1[dyadDist < 250])
+
+rmNN.dyadDist.id1.d250<-
+  glm(dyadDist ~ preyRSF*predatorRSF,
+      data = rmnp.dyad1[dyadDist < 250])
+
+rmNN.dyadDist.id2.d250<-
+  glm(dyadDist ~ rpreyRSF*rpredatorRSF,
+      data = rmnp.dyad1[dyadDist < 250])
+
+summary(rmNN.dyadDist.avg.d250)
+summary(rmNN.dyadDist.id1.d250)
+summary(rmNN.dyadDist.id2.d250)
+
+rmNN.dAbsAng.avg.d500<-
+  glm(dAbsAng ~ avg.PreyRSF*avg.PredRSF,
+      data = rmnp.dyad1[dyadDist < 500])
+
+rmNN.dAbsAng.id1.d500<-
+  glm(dAbsAng ~ preyRSF*predatorRSF,
+      data = rmnp.dyad1[dyadDist < 500])
+
+rmNN.dAbsAng.id2.d500<-
+  glm(dAbsAng ~ rpreyRSF*rpredatorRSF,
+      data = rmnp.dyad1[dyadDist < 500])
+
+summary(rmNN.dAbsAng.avg.d500)
+summary(rmNN.dAbsAng.id1.d500)
+summary(rmNN.dAbsAng.id2.d500)
+
+rmNN.dAbsAng.avg.d50<-
+  glm(dAbsAng ~ avg.PreyRSF*avg.PredRSF,
+      data = rmnp.dyad1[dyadDist < 50])
+
+rmNN.dAbsAng.id1.d50<-
+  glm(dAbsAng ~ preyRSF*predatorRSF,
+      data = rmnp.dyad1[dyadDist < 50])
+
+rmNN.dAbsAng.id2.d50<-
+  glm(dAbsAng ~ rpreyRSF*rpredatorRSF,
+      data = rmnp.dyad1[dyadDist < 50])
+
+summary(rmNN.dAbsAng.avg.d50)
+summary(rmNN.dAbsAng.id1.d50)
+summary(rmNN.dAbsAng.id2.d50)
 
 rmNNSpring <-
-  glm(absAngle ~ elkspring + wolfspring + elkspring:wolfspring,
+  glm(dSI ~ elkspring + wolfspring + elkspring:wolfspring,
       data = rmnp[season == 'spring'])
 
 #turning angle
