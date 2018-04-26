0fd3fd89062bd1eda8bcea0017d1d29676f2012f
Alec Robitaille <robit.alec@gmail.com>
2018-04-26 10:38:54 -0230

    lead relative angle

--- R/0-functions/RelativeAngle.R
+++ R/0-functions/RelativeAngle.R
@@ -1,9 +1,9 @@
 RelativeAngle <- function(DT, abs = NULL, coord = NULL, datetime = NULL, 
                           id = NULL, allCW = FALSE, returnIntermediate = FALSE) {
   if('absAngle' %in% colnames(DT)){
-    DT[, relAngle := absAngle - shift(absAngle, 1, type = 'lag')]
+    DT[, relAngle := absAngle - shift(absAngle, 1, type = 'lead')]
   } else {
     AbsoluteAngle(DT, coord, datetime, id, yr, allCW, returnIntermediate)
-    DT[, relAngle := absAngle - shift(absAngle, 1, type = 'lag')]
+    DT[, relAngle := absAngle - shift(absAngle, 1, type = 'lead')]
   }
 }
