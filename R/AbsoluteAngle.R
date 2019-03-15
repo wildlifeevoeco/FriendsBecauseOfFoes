030a3ecfc75047cf1f29106e589194fc2e6359e6
Alec Robitaille <robit.alec@gmail.com>
2019-03-15 10:19:39 -0700

    move functions to R

---
+++ R/AbsoluteAngle.R
@@ -0,0 +1,48 @@
+#' Absolute angle
+#'
+#' @param DT data.table
+#' @param coords character vector; coordinate columns
+#' @param datetime character; datetime column
+#' @param id character; id column
+#' @param yr character; year column
+#' @param allCW boolean; all clockwise
+#' @param returnIntermediate boolean; return intermediate columns
+#'
+#' @return input DT appended with absolute angle column 
+#' 
+#' @export
+#'
+#' @examples
+AbsoluteAngle <- function(DT,
+                          coords,
+                          datetime,
+                          id,
+                          yr,
+                          allCW = FALSE,
+                          returnIntermediate = FALSE) {
+  # Create lag and dif column names
+  lagCols <- paste('lag', coords, sep = '')
+  difCols <- c('difX', 'difY')
+  
+  DT[order(datetime),
+     (lagCols) := data.table::shift(.SD, 1, NA, 'lead'),
+     by = c(id, yr), .SDcols = coords]
+  
+  # Find the difference between subsequent points in x,y
+  DT[, (difCols) := .((get(lagCols[1]) - get(coords[1])),
+                      (get(lagCols[2]) - get(coords[2])))]
+  
+  DT[, absAngle := atan2(difY, difX) * 180 / pi]
+  
+  if (!returnIntermediate) {
+    DT[, c(lagCols, difCols) := NULL]
+  }
+  
+  # TODO: what did this mean? .. my poor code commenting
+  if (!allCW) {
+    DT
+  } else {
+    DT[absAngle < 0, absAngle := absAngle + 360]
+  }
+  
+}
