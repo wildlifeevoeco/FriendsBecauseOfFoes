ea6dddc775f05cc6d691fe21a3622b07c53bbaf6
Alec Robitaille <robit.alec@gmail.com>
2019-03-15 10:30:15 -0700

    add data.table imports in prep_date

--- R/prep_date.R
+++ R/prep_date.R
@@ -12,24 +12,24 @@
 #' @examples
 prep_date <- function(DT, dateCol, timeCol, dateFormat = NULL, timeFormat = NULL) {
   if(is.null(dateFormat)) {
-    DT[, idate := as.IDate(get(dateCol))]
+    DT[, idate := data.table::as.IDate(get(dateCol))]
   } else {
-    DT[, idate := as.IDate(get(dateCol), format = dateFormat)]
+    DT[, idate := data.table::as.IDate(get(dateCol), format = dateFormat)]
   }
   
   if(is.null(timeFormat)) {
-    DT[, itime := as.ITime(get(timeCol))]
+    DT[, itime := data.table::as.ITime(get(timeCol))]
   } else {
-    DT[, itime := as.ITime(get(timeCol), format = timeFormat)]
+    DT[, itime := data.table::as.ITime(get(timeCol), format = timeFormat)]
   }
   
   DT[, datetime := as.POSIXct(paste(idate, itime), format = '%F %T')]
   
-  DT[, julday := yday(idate)]
-  DT[, yr := year(idate)]
-  DT[, mnth := month(idate)]
+  DT[, julday := data.table::yday(idate)]
+  DT[, yr := data.table::year(idate)]
+  DT[, mnth := data.table::month(idate)]
   
-  DT[, hr := hour(itime)]
+  DT[, hr := data.table::hour(itime)]
   
   if(all(c('idate', 'itime', 'datetime', 'julday', 'yr', 'mnth') %in% colnames(DT))){ 
     message('date and time fields added successfully')
