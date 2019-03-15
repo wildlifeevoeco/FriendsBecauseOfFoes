627a9156d40693e6b6c07b14d8f016660ad60eca
Alec Robitaille <robit.alec@gmail.com>
2019-03-15 12:30:03 -0700

    add imports generate_grid

--- R/generate_grid.R
+++ R/generate_grid.R
@@ -1,29 +1,53 @@
-GenerateGrid <- function(spacing, crs, mcpExtent, DT = NULL, xCol = NULL, yCol = NULL){
-  
-  extentDT <- data.table(mcpExtent@bbox, keep.rownames = TRUE)
-  
-  extentDT[, dif := abs(min - max)]  
-  
-  
-  if(extentDT[order(-dif)][1, rn] == "x"){
-    xSeq <- extentDT[rn == "x", seq(min, max, by = spacing)]
-    ySeq <- extentDT[rn == "y", seq(min, min + (spacing * (length(xSeq) - 1)),
-                                    by = spacing)]
-  } else {
-    ySeq <- extentDT[rn == "y", seq(min, max, by = spacing)]
-    xSeq <- extentDT[rn == "x", seq(min, min + (spacing * (length(ySeq) - 1)),
-                                    by = spacing)]
+#' Generate grid
+#'
+#' @param spacing
+#' @param crs
+#' @param mcpExtent
+#' @param DT
+#' @param xCol
+#' @param yCol
+#'
+#' @return
+#' @export
+#'
+#' @examples
+generate_grid <-
+  function(spacing,
+           crs,
+           mcpExtent,
+           DT = NULL,
+           xCol = NULL,
+           yCol = NULL) {
+    rn <- n <- NULL
+    extentDT <- data.table::data.table(mcpExtent@bbox, keep.rownames = TRUE)
+    
+    extentDT[, dif := abs(min - max)]
+    
+    
+    if (extentDT[order(-dif)][1, rn] == "x") {
+      xSeq <- extentDT[rn == "x", seq(min, max, by = spacing)]
+      ySeq <-
+        extentDT[rn == "y", seq(min, min + (spacing * (length(xSeq) - 1)),
+                                by = spacing)]
+    } else {
+      ySeq <- extentDT[rn == "y", seq(min, max, by = spacing)]
+      xSeq <-
+        extentDT[rn == "x", seq(min, min + (spacing * (length(ySeq) - 1)),
+                                by = spacing)]
+    }
+    
+    r <- raster::extent(head(xSeq, n = 1),
+                        tail(xSeq, n = 1),
+                        head(ySeq, n = 1),
+                        tail(ySeq, n = 1))
+    ra <- raster::raster(r)
+    raster::res(ra) <- c(spacing, spacing)
+    
+    raster::projection(ra) <- sp::CRS(crs)
+    
+    rSP <- raster::rasterToPoints(ra, spatial = TRUE)
+    
+    data.table::data.table(
+      rSP@coords, n = 1:length(rSP))[
+        n %in% sp::over(mcpExtent, rSP, returnList = TRUE)[[1]]][, n := NULL]
   }
-  
-  r <- raster::extent(head(xSeq, n = 1), tail(xSeq, n = 1), 
-                      head(ySeq, n = 1), tail(ySeq, n = 1))
-  ra <- raster(r)
-  res(ra) <- c(spacing, spacing)
-  
-  projection(ra) <- CRS(crs)
-  
-  rSP <- rasterToPoints(ra, spatial = TRUE)
-  
-  data.table(rSP@coords, n = 1:length(rSP))[n %in% over(mcpExtent, rSP, returnList = TRUE)[[1]]][, n := NULL]
-  
-}
