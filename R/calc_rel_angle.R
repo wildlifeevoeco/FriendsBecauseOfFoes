91a718975163393e15b913a15ab1da6fbb1d4d10
Alec Robitaille <robit.alec@gmail.com>
2019-03-15 10:26:21 -0700

    rename RelativeAngle -> calc_rel_angle

---
+++ R/calc_rel_angle.R
@@ -0,0 +1,23 @@
+#' Relative Angles
+#'
+#' @param DT 
+#' @param abs 
+#' @param coord 
+#' @param datetime 
+#' @param id 
+#' @param allCW 
+#' @param returnIntermediate 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+calc_rel_angle <- function(DT, abs = NULL, coord = NULL, datetime = NULL, 
+                          id = NULL, allCW = FALSE, returnIntermediate = FALSE) {
+  if('absAngle' %in% colnames(DT)){
+    DT[, relAngle := data.table::shift(absAngle, 1, type = 'lead') - absAngle]
+  } else {
+    calc_abs_angle(DT, coord, datetime, id, yr, allCW, returnIntermediate)
+    DT[, relAngle := data.table::shift(absAngle, 1, type = 'lead') - absAngle]
+  }
+}
