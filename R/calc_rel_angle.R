b1225876f2c46e04194557ba0041a355a22a1a0b
Alec Robitaille <robit.alec@gmail.com>
2019-03-15 10:58:47 -0700

    fix match coords from abs to rel

--- R/calc_rel_angle.R
+++ R/calc_rel_angle.R
@@ -1,23 +1,29 @@
 #' Relative Angles
 #'
-#' @param DT 
-#' @param abs 
-#' @param coord 
-#' @param datetime 
-#' @param id 
-#' @param allCW 
-#' @param returnIntermediate 
+#' @inheritParams calc_abs_angle
+#' @param abs
+#' @param coords
+#' @param datetime
+#' @param id
+#' @param allCW
+#' @param returnIntermediate
 #'
 #' @return
 #' @export
 #'
 #' @examples
-calc_rel_angle <- function(DT, abs = NULL, coord = NULL, datetime = NULL, 
-                          id = NULL, allCW = FALSE, returnIntermediate = FALSE) {
-  if('absAngle' %in% colnames(DT)){
-    DT[, relAngle := data.table::shift(absAngle, 1, type = 'lead') - absAngle]
-  } else {
-    calc_abs_angle(DT, coord, datetime, id, yr, allCW, returnIntermediate)
-    DT[, relAngle := data.table::shift(absAngle, 1, type = 'lead') - absAngle]
+calc_rel_angle <-
+  function(DT,
+           abs = NULL,
+           coords = NULL,
+           datetime = NULL,
+           id = NULL,
+           allCW = FALSE,
+           returnIntermediate = FALSE) {
+    if ('absAngle' %in% colnames(DT)) {
+      DT[, relAngle := data.table::shift(absAngle, 1, type = 'lead') - absAngle]
+    } else {
+      calc_abs_angle(DT, coord, datetime, id, yr, allCW, returnIntermediate)
+      DT[, relAngle := data.table::shift(absAngle, 1, type = 'lead') - absAngle]
+    }
   }
-}
