10fcb0748bf500b32e4fe06eb36bb593037d1c3e
Alec Robitaille <robit.alec@gmail.com>
2019-04-08 10:14:19 -0700

    update extent path

--- scripts/0-variables/variables.R
+++ scripts/0-variables/variables.R
@@ -11,14 +11,14 @@ spring <- c(141, 212)
 utmNL <- '+proj=utm +zone=21 ellps=WGS84'
 
 # NL Bounds shapefile
-nlBounds <- spTransform(readOGR('input/etc/NL/NL-Bounds.shp'),
+nlBounds <- spTransform(readOGR('input/extent/NL/NL-Bounds.shp'),
                         CRSobj = utmNL)
 
 # UTM zone 14N
 utmMB <- '+proj=utm +zone=14 +ellps=WGS84 +datum=WGS84 +units=m +no_defs'
 
 # MB Bounds shapefile
-mbBounds <- spTransform(readOGR('input/etc/RMNP/RMNPextent.shp'),
+mbBounds <- spTransform(readOGR('input/extent/RMNP/RMNPextent.shp'),
                         CRSobj = utmMB)
 
 ### Set output columns (for prep) ----
