36fdcd378f500c9f8645b77b1263ae12e75acef3
Alec Robitaille <robit.alec@gmail.com>
2019-04-08 10:24:56 -0700

    mv ruggedness separate script to raster prep

--- scripts/1-data-prep/RasterPrep.R
+++ scripts/1-data-prep/RasterPrep.R
@@ -1,10 +1,10 @@
 ### Raster Processing MB ----
 # Authors: Alec Robitaille, Christina M Prokopenko, Sana Zabihi
-# Purpose: 
+# Purpose:
 # Inputs: Elk relocation data
-# Outputs: 
+# Outputs:
 # Project: Easter Week Challenge 2018
-# Copyright: ./LICENSE.md 
+# Copyright: ./LICENSE.md
 
 
 ### Packages ----
@@ -16,27 +16,51 @@ lapply(libs, require, character.only = TRUE)
 source('scripts/0-variables/variables.R')
 
 
-### Input data ----
+### Calculate ruggedness ----
+# Read in DEM
+DEM <- raster('input/covariates/RMNP/Elevation.tif')
+
+ruggedness <- terrain(elevation, opt = "roughness")
+
+writeRaster(
+  ruggedness,
+  'input/covariates/RMNP/Ruggedness',
+  format = 'GTiff',
+  overwrite = T
+)
+
+
+
+### List rasters ----
 # Covariates
-lsCovers <- data.table(nm = dir('input/covariates/RMNP', '.tif$'))[, nm := gsub(".tif|100m", "", nm)]$nm
+lsCovers <-
+  data.table(nm = dir('input/covariates/RMNP', '.tif$'))
+lsCovers <- gsub(".tif|100m", "", lsCovers$nm)
 lsPaths <- dir('input/covariates/RMNP', '.tif$', full.names = TRUE)
 names(lsPaths) <- lsCovers
 
 ### Processing ----
 # Crop the rasters, holding as temp files in a list
-cropRasters <- lapply(lsPaths, FUN = function(r){
-  crop(raster(r), mbBounds)
-})
+cropRasters <- lapply(
+  lsPaths,
+  FUN = function(r) {
+    crop(raster(r), mbBounds)
+  }
+)
 
 # Log transform
 namesTransform <- c('LinFeat_Dist', 'Water_Dist')
-rasterTransform <- cropRasters[lapply(cropRasters, names) %in% namesTransform]
+rasterTransform <-
+  cropRasters[lapply(cropRasters, names) %in% namesTransform]
 
-transformed <- lapply(seq_along(namesTransform), FUN = function(x){
-   r <- log(rasterTransform[[x]] + 1)
-   names(r) <- namesTransform[[x]]
-   r
-})
+transformed <- lapply(
+  seq_along(namesTransform),
+  FUN = function(x) {
+    r <- log(rasterTransform[[x]] + 1)
+    names(r) <- namesTransform[[x]]
+    r
+  }
+)
 names(transformed) <- namesTransform
 
 transformed[['LinFeat_Dist']] <-
@@ -50,12 +74,18 @@ cropRasters[['Elevation']] <-
   resample(cropRasters[['Elevation']], cropRasters[['Water_Dist']])
 
 ### Output ----
-outRaster <- c(transformed, cropRasters[!(lapply(cropRasters, names) %in% namesTransform)])
+outRaster <- c(transformed,
+               cropRasters[!(lapply(cropRasters, names) %in% namesTransform)])
 outNames <- lapply(outRaster, names)
 
-lapply(seq_along(outRaster), FUN = function(r){
-  writeRaster(outRaster[[r]], paste0('output/1-data-prep/covariates/RMNP/', outNames[[r]]), 
-              format = 'GTiff', 
-              overwrite = T)
-})
-
+lapply(
+  seq_along(outRaster),
+  FUN = function(r) {
+    writeRaster(
+      outRaster[[r]],
+      paste0('output/1-data-prep/covariates/RMNP/', outNames[[r]]),
+      format = 'GTiff',
+      overwrite = T
+    )
+  }
+)
