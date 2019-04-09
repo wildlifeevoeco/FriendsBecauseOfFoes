80d530163f7e45f6d576d43ac76c5f33d1b4955e
Alec Robitaille <robit.alec@gmail.com>
2019-04-08 17:39:23 -0700

    rm duplicated calc turn angles

--- scripts/4-sociality/CalculateTurnAngles.R
+++ scripts/4-sociality/CalculateTurnAngles.R
@@ -1,26 +1,24 @@
 ### Calculate Turn Angles ----
 # Authors: Alec Robitaille
-# Started: March 15 2018
-# Purpose: Calculate absolute and relative turn angle 
-#          between locs of an individual
-# Inputs: Elk, caribou relocation data
-# Outputs: Abs TA data
-
 
 
 #TODO: Grab Hance's updated social metrics from modeling script
 
 ### Packages ----
-libs <- c('data.table', 'ggplot2',
-          'SearchTrees',
-          'magrittr')
+libs <- c('data.table', 'ggplot2', 'ewc', 'SearchTrees')
 lapply(libs, require, character.only = TRUE)
+
+
+### Set variables ----
+source('scripts/0-variables/variables.R')
 
 
 ### Input data ----
 # Which species would you like to calculate abs and rel TA for?
 species <- 'elk'
-DT <- readRDS(paste0('output/rsf-values/', species, 'RsfValues.Rds'))
+DT <- readRDS(paste0('output/3-extraction/', species, 'RsfValues.Rds'))
+
+if (truelength(DT) == 0) alloc.col(DT) 
 
 # List relevant column names
 coordCols <- c('EASTING', 'NORTHING')
@@ -29,49 +27,18 @@ datetimeCol <- 'datetime'
 yrCol <- 'yr'
 
 ### Variables ----
+# TODO: check rleid/ http://stackoverflow.com/q/21421047/559784
 DT[, rowID := rleid(EASTING), by = c("id", "yr")]
 
 ### Calculate Absolute Angle ----
-
+# TODO: (Ask Hance) would it be better if we had range from 0-360 instead? for dif abs angle
 calc_abs_angle(DT, coordCols, datetimeCol, idCol, yrCol, FALSE, FALSE)
 
 #TODO: check that yrcol is well handled
 calc_rel_angle(DT, coordCols, datetimeCol, idCol, yrCol, FALSE, FALSE)
 
 ### Output ----
-saveRDS(DT, paste0('output/angles/', species, 'Angle.Rds'))
-
-### Figures ----
-qplot(absAngle, data = DT)
-
-
-
-
-
-
-### Input data ----
-# Which species would you like to calculate abs and rel TA for?
-species <- 'Caribou'
-DT <- readRDS(paste0('output/rsf-values/', species, 'RSFVals.Rds'))
-
-# List relevant column names
-coordCols <- c('EASTING', 'NORTHING')
-idCol <- 'id'
-datetimeCol <- 'datetime' 
-yrCol <- 'yr'
-
-### Variables ----
-DT[, rowID := rleid(EASTING), by = c(idCol, yrCol)]
-
-### Calculate Absolute Angle ----
-
-calc_abs_angle(DT, coordCols, datetimeCol, idCol, yrCol, FALSE, FALSE)
-
-
-calc_rel_angle(DT, coordCols, datetimeCol, idCol, yrCol, FALSE, FALSE)
-
-### Output ----
-saveRDS(DT, paste0('output/angles/', species, 'AngleNEW.Rds')) ### New coyote RSF
+saveRDS(DT, paste0('output/4-angles/', species, 'Angle.Rds'))
 
 ### Figures ----
 qplot(absAngle, data = DT)
