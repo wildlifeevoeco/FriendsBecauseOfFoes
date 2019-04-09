281376f4b6ea95bee5c92e42e94b086d787a2807
Alec Robitaille <robit.alec@gmail.com>
2019-04-09 11:00:12 -0700

    start nl rsf rerun

--- scripts/2-rsf/NL/BearRSF.R
+++ scripts/2-rsf/NL/BearRSF.R
@@ -5,33 +5,120 @@
 
 #TODO: it's spring and not summer right?
 ### Packages ----
-libs <- c('data.table', 'sp', 'adehabitatHR')
+libs <- c('data.table', 'sp', 'adehabitatHR', 'raster',
+          'ewc')
 # libs <- c('adehabitatHR', 'rgeos', 'spatstat', 'raster',
 #           'ewc', 
 #           'data.table', 'piecewiseSEM', 'rgdal')
 lapply(libs, require, character.only = TRUE)
 
+
+### Set variables ----
+source('scripts/0-variables/variables.R')
+
+
 ### Input data ----
-caribou <- readRDS('output/1-data-prep/caribou.Rds')
+bear <- readRDS('output/1-data-prep/bear.Rds')
+
+# Covariates
+lsCovers <- gsub(".tif|100m|prep", "", 
+               dir('output/1-data-prep/covariates/NL', '.tif$'))
+
+lsPaths <- dir('output/1-data-prep/covariates/NL', 
+             '.tif$', full.names = TRUE)
+names(lsPaths) <- lsCovers
 
 
 ### Processing ----
-points <- SpatialPoints(caribou[, .(EASTING, NORTHING)],
-                        proj4string = CRS(utm))
+points <- SpatialPoints(bear[, .(EASTING, NORTHING)],
+                        proj4string = CRS(utmNL))
 
 mcps <- mcp(points, percent = 100)
 
 
-### Buffer by mean step length
-CarAvailBuf <- gBuffer(CarAvail, width = mean(caribou$stepLength))
+# Create Regular Grid
+regPts <- generate_grid(mcps, 90, crs = utmNL)
+setnames(regPts, c('EASTING', 'NORTHING'))
 
-Carclipped <- gIntersection(nlBounds, CarAvailBuf)
-Carclipped2 <- as.owin.SpatialPolygons(Carclipped)
+# Combine observed and regular grid points
+regPts[, observed := 0]
+bear[, observed := 1]
 
-#### Load in habitat layers
+# Add fake season to regular grid 'grid'
+regPts[, season := 'grid']
+
+samplePts <- rbindlist(list(regPts, bear), 
+                       use.names = TRUE, fill = TRUE)
 
 
+### Sampling ----
+# Drop columns leaving only needed
+cols <- c('id','EASTING', 'NORTHING', 'season', 'observed')
+samplePts <- samplePts[, ..cols]
 
+# Add row ID
+samplePts[, rowID := .I]
+
+# Sample rasters
+lsRasters <- lapply(lsPaths, raster)
+
+samplePts[, (lsCovers) := lapply(
+  lsRasters,
+  FUN = function(r) {
+    extract(r, matrix(c(EASTING, NORTHING), ncol = 2))
+  }
+)]
+
+
+### RSF ----
+# Winter RSF
+winterPts <- samplePts[season == "winter" | season == 'grid']
+winterPts[season == 'grid', season := "winter"]
+
+winterRSF <- glm(reformulate(lsCovers, response = 'observed'), 
+                 family = 'binomial',data = winterPts)
+
+summary(winterRSF)
+vif(winterRSF)
+rsquared(winterRSF)
+
+# Pull out the coefficients, dropping the intercept
+winterCoefs <- coef(winterRSF)[-1]
+
+
+# Create the raster matching the first raster layer with the first fixed effect
+intercept <- coef(winterRSF)[1]
+
+if (all(names(winterCoefs) == names(lsRasters))) {
+  winterRaster <-
+    exp(intercept + Reduce('+', Map('*', winterCoefs, lsRasters)))
+} else {
+  stop('names dont match, check coef and rasters')
+}
+
+# Spring RSF
+springwolf <- samplePts[season == "spring" | season == 'grid']
+springwolf[observed == 0, season := "spring"]
+
+springwolfRSF <- glm(reformulate(lsCovers, response = 'observed'), 
+                     family = 'binomial',data = springwolf)
+
+summary(springwolfRSF)
+vif(springwolfRSF)
+rsquared(springwolfRSF)
+
+# Pull out the coefficients, dropping the intercept
+springCoefs <- coef(springwolfRSF)[-1]
+
+# Create the raster matching the first raster layer with the first fixed effect
+intercept <- coef(winterwolfRSF)[1]
+
+if (all(names(winterCoefs) == names(lsRasters))) {
+  springRaster <-
+    exp(intercept + Reduce('+', Map('*', springCoefs, lsRasters)))
+} else {
+  stop('names dont match, check coef and rasters')
+}
 
 bears <- readRDS('output/1-data-prep/bear.Rds')
 nrow(bears)
