7e1d7547d6735e7c5cee82d22376f4c70b9ad713
Alec Robitaille <robit.alec@gmail.com>
2019-04-08 17:52:14 -0700

    cleaner object naming elk extraction

--- scripts/3-extraction/Elk-Extraction.R
+++ scripts/3-extraction/Elk-Extraction.R
@@ -17,25 +17,25 @@ source('scripts/0-variables/variables.R')
 elk <- readRDS('output/1-data-prep/elk.Rds')
 
 # RSFs
-wolfwinter.rsf <- raster('output/2-rsf/wolf/wolfrsfWinter.tif')
-wolfspring.rsf <- raster('output/2-rsf/wolf/wolfrsfSpring.tif')
-elkwinter.rsf <- raster('output/2-rsf/elk/elkrsfWinter.tif')
-elkspring.rsf <- raster('output/2-rsf/elk/elkrsfSpring.tif')
+wolfWinter <- raster('output/2-rsf/wolf/wolfrsfWinter.tif')
+wolfSpring <- raster('output/2-rsf/wolf/wolfrsfSpring.tif')
+elkWinter <- raster('output/2-rsf/elk/elkrsfWinter.tif')
+elkSpring <- raster('output/2-rsf/elk/elkrsfSpring.tif')
 
-lsrasters <- list(wolfwinter.rsf, wolfspring.rsf, elkwinter.rsf, elkspring.rsf)
-lsnames <- c('wolfwinter', 'wolfspring', 'elkwinter', 'elkspring')
+rasters <- list(wolfWinter, wolfSpring, elkWinter, elkSpring)
+names <- c('wolfwinter', 'wolfspring', 'elkwinter', 'elkspring')
 
 ### Sampling ----
-# Sample rasters
-elk[, (lsnames) := lapply(lsrasters, FUN = function(r){
-  extract((r), matrix(c(EASTING, NORTHING), ncol = 2))})]
+elk[, (names) := lapply(rasters, FUN = function(r){
+  extract(r, matrix(c(EASTING, NORTHING), ncol = 2))})]
 
 elk[season == 'winter', predatorRSF := wolfwinter]
 elk[season == 'winter', preyRSF := elkwinter]
 elk[season == 'spring', predatorRSF := wolfspring]
 elk[season == 'spring', preyRSF := elkspring]
 
-elk[, (lsnames) := NULL]
+elk[, (names) := NULL]
 
 ### Save output ----
+# TODO: rename output "rsfvalues"
 saveRDS(elk, 'output/3-extraction/elkRsfValues.Rds')
