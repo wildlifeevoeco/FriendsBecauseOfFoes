11009b843e51044ecdf75de518559863558b0f63
Alec Robitaille <robit.alec@gmail.com>
2019-04-09 16:13:37 -0700

    tidy coyote rsf

--- scripts/2-rsf/NL/CoyoteRSF.R
+++ scripts/2-rsf/NL/CoyoteRSF.R
@@ -1,422 +1,112 @@
 ### Coyote RSF ----
 # Authors: Michel Laforge, Alec Robitaille
 
-# Copyright: ./LICENSE.md
 
-#TODO: it's spring and not summer right?
 ### Packages ----
-libs <- c(
-  'adehabitatHR',
-  'rgeos',
-  'spatstat',
-  'polyCub',
-  'raster',
-  'data.table',
-  'ewc',
-  'piecewiseSEM',
-  'rgdal'
-)
+libs <- c('data.table', 'sp', 'adehabitatHR', 'raster',
+          'ewc', 'rgeos', 'lme4', 'car','piecewiseSEM')
+# libs <- c('adehabitatHR', 'rgeos', 'spatstat', 'raster',
+#           'ewc', 
+#           'data.table', 'piecewiseSEM', 'rgdal')
 lapply(libs, require, character.only = TRUE)
 
+
+### Set variables ----
+source('scripts/0-variables/variables.R')
+
+
 ### Input data ----
-
-utm <- '+proj=utm +zone=21 ellps=WGS84'
-nlBounds <- spTransform(readOGR('input/etc/NL/NL-Bounds.shp'),
-                        CRSobj = utm)
-
-############ caribou
-
-
-MRcar <- readRDS('output/1-data-prep/caribou.Rds')
-
-
-plot(nlBounds)
-points(MRcar$EASTING, MRcar$NORTHING)
-
-CarPoints <-
-  SpatialPoints(data.frame(MRcar$EASTING, MRcar$NORTHING), proj4string = CRS(utm))
-
-CarAvail <- mcp(CarPoints, percent = 100)
-shapefile(CarAvail, "Output/caribouMCP.shp")
-
-plot(nlBounds)
-plot(CarAvail, add = T)
-points(CarPoints)
-
-### Buffer by mean step length
-CarAvailBuf <- gBuffer(CarAvail, width = mean(MRcar$stepLength))
-
-Carclipped <- gIntersection(nlBounds, CarAvailBuf)
-Carclipped2 <- as.owin.SpatialPolygons(Carclipped)
-
-#### Load in habitat layers
-
-Ant <- raster('input/Landcover/Reproj/Anthro100.tif')
-Bro <- raster('input/Landcover/Reproj/Broadleaf100.tif')
-Con <- raster('input/Landcover/Reproj/Conifer100.tif')
-Lic <- raster('input/Landcover/Reproj/Lichen100.tif')
-Mix <- raster('input/Landcover/Reproj/MixedWood100.tif')
-Roc <- raster('input/Landcover/Reproj/Rocky100.tif')
-Scr <- raster('input/Landcover/Reproj/Scrub100.tif')
-Wat <- raster('input/Landcover/Reproj/Water100.tif')
-Wet <- raster('input/Landcover/Reproj/Wetland100.tif')
-WaD1 <- raster('input/Landcover/Reproj/WaterDist.tif')
-Lin1 <- raster('input/Landcover/Reproj/LinearDist.tif')
-
-WaD <- log(WaD1 + 1)
-Lin <- log(Lin1 + 1)
-
-Elev <- raster('input/Landcover/Reproj/NLElev.tif')
-Rug <- terrain(Elev, opt = "roughness")
-
-AntCrop <- crop(Ant, Carclipped)
-BroCrop <- crop(Bro, Carclipped)
-ConCrop <- crop(Con, Carclipped)
-LicCrop <- crop(Lic, Carclipped)
-MixCrop <- crop(Mix, Carclipped)
-RocCrop <- crop(Roc, Carclipped)
-ScrCrop <- crop(Scr, Carclipped)
-WatCrop <- crop(Wat, Carclipped)
-WetCrop <- crop(Wet, Carclipped)
-RugCrop <- crop(Rug, Carclipped)
-WaDCrop <- crop(WaD, Carclipped)
-LinCrop <- crop(Lin, Carclipped)
-
-AntR <- AntCrop
-BroR <- resample(BroCrop, AntCrop)
-ConR <- resample(ConCrop, AntCrop)
-LicR <- resample(LicCrop, AntCrop)
-MixR <- resample(MixCrop, AntCrop)
-RocR <- resample(RocCrop, AntCrop)
-ScrR <- resample(ScrCrop, AntCrop)
-WaDR <- resample(WaDCrop, AntCrop)
-LinR <- resample(LinCrop, AntCrop)
-RugR <- resample(RugCrop, AntCrop)
-
-
 coyote <- readRDS('output/1-data-prep/coyote.Rds')
-nrow(coyote)
-plot(coyote$EASTING, coyote$NORTHING)
 
-head(coyote)
+# Covariates
+covers <- gsub(".tif|100|prep", "", 
+               dir('output/1-data-prep/covariates/NL', '.tif$'))
+paths <- dir('output/1-data-prep/covariates/NL', 
+             '.tif$', full.names = TRUE)
+names(paths) <- covers
 
+rmList <- which(covers %in% c('Water', 'NLElev'))
 
-plot(nlBounds)
-points(coyote$EASTING, coyote$NORTHING)
-
-unique(MRcoy$herd)
-str(coyote)
-
-plot(nlBounds)
-#points(MRcoy$EASTING,MRcoy$NORTHING)
+lsCovers <- covers[-rmList]
+lsPaths <- paths[-rmList]
 
 
-coyote$STATUS <- "RESIDENT"
-coyote$STATUS <-
-  ifelse(coyote$id == "co_lp1109", "UNKNOWN", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_mr1003", "UNKNOWN", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_mr1012", "UNKNOWN", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_np1022", "UNKNOWN", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_lp0902", "SUB-TRANSIENT", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_lp1001", "SUB-TRANSIENT", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_lp1003", "SUB-TRANSIENT", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_lp1012", "SUB-TRANSIENT", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_lp1107", "SUB-TRANSIENT", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_mr1005", "SUB-TRANSIENT", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_mr1010", "SUB-TRANSIENT", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_mr1312", "SUB-TRANSIENT", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_mr1303", "SUB-TRANSIENT", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_np0915", "SUB-TRANSIENT", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_np1005", "SUB-TRANSIENT", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_np1006", "SUB-TRANSIENT", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_np1017", "SUB-TRANSIENT", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_lp1005", "TOO FEW DAYS", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_lp1106", "TOO FEW DAYS", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_mr0806", "TOO FEW DAYS", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_mr0901", "TOO FEW DAYS", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_mr0907", "TOO FEW DAYS", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_mr0911", "TOO FEW DAYS", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_mr0914", "TOO FEW DAYS", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_mr1014", "TOO FEW DAYS", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_mr1015", "TOO FEW DAYS", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_mr1102", "TOO FEW DAYS", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_mr1106", "TOO FEW DAYS", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_mr1205", "TOO FEW DAYS", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_mr1304", "TOO FEW DAYS", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_mr1311", "TOO FEW DAYS", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_mr1313", "TOO FEW DAYS", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_mr1314", "TOO FEW DAYS", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_np0801", "TOO FEW DAYS", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_np0904", "TOO FEW DAYS", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_np1002", "TOO FEW DAYS", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_np1015", "TOO FEW DAYS", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_np1014", "TOO FEW DAYS", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_lp0908", "TOO FEW LOCS", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_lp1010", "TOO FEW LOCS", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_lp1013", "TOO FEW LOCS", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_lp1101", "TOO FEW LOCS", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_mr1009", "TOO FEW LOCS", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_mr1107", "TOO FEW LOCS", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_mr1108", "TOO FEW LOCS", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_mr1204", "TOO FEW LOCS", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_mr1210", "TOO FEW LOCS", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_mr1211", "TOO FEW LOCS", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_np0908", "TOO FEW LOCS", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_np1021", "TOO FEW LOCS", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_lp0904", "TRANSIENT", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_lp0910", "TRANSIENT", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_lp1002", "TRANSIENT", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_lp1006", "TRANSIENT", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_lp1008", "TRANSIENT", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_lp1014", "TRANSIENT", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_mr0905", "TRANSIENT", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_mr0906", "TRANSIENT", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_mr0908", "TRANSIENT", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_mr1001", "TRANSIENT", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_mr1004", "TRANSIENT", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_mr1006", "TRANSIENT", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_mr1007", "TRANSIENT", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_mr1011", "TRANSIENT", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_mr1013", "TRANSIENT", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_mr1101", "TRANSIENT", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_mr1105", "TRANSIENT", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_mr1209", "TRANSIENT", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_mr1212", "TRANSIENT", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_mr1302", "TRANSIENT", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_mr1306", "TRANSIENT", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_mr1308", "TRANSIENT", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_np0903", "TRANSIENT", coyote$STATUS)
-coyote$STATUS <-
-  ifelse(coyote$id == "co_np1016", "TRANSIENT", coyote$STATUS)
+### Processing ----
+points <- SpatialPoints(coyote[, .(EASTING, NORTHING)],
+                        proj4string = CRS(utmNL))
+width <- coyote[, mean(stepLength)]
+buffers <- buffer(points, width = width)
+intersect <- gIntersection(nlBounds, gUnaryUnion(buffers))
 
-coyMR <- subset(coyote, herd == "MIDRIDGE")
-coyMRres <-
-  subset(coyMR, STATUS == "RESIDENT" | STATUS == "SUB-TRANSIENT")
-
-saveRDS(coyMRres, "output/ResidentCoyotes.RDS")
-
-nrow(coyMRres)
-CoyPoints <-
-  SpatialPoints(data.frame(coyMRres$EASTING, coyMRres$NORTHING),
-                proj4string = CRS(utm))
-
-CoyAvail <- mcp(CoyPoints, percent = 100)
-shapefile(CoyAvail, "output/coyoteMCP.shp", overwrite = T)
-
-### Buffer by mean step length
-CoyAvailBuf <- gBuffer(CoyAvail, width = mean(coyMRres$stepLength))
-
-Coyclipped <- gIntersection(nlBounds, CoyAvailBuf)
-#clipped2<-as.owin.SpatialPolygons(clipped)
+buffered <- buffer(points, width = coyote[, mean(stepLength)])
 
 # Create Regular Grid
+regPts <- generate_grid(intersect, 90, crs = utmNL)
+setnames(regPts, c('EASTING', 'NORTHING'))
 
-regPts <- generate_grid(Coyclipped, 90, crs = utm)
+# Combine observed and regular grid points
+regPts[, observed := 0]
+coyote[, observed := 1]
 
+# Add fake season to regular grid 'grid'
+regPts[, season := 'grid']
 
-
-CoyAntUsed <- extract(Ant, CoyPoints)
-CoyBroUsed <- extract(Bro, CoyPoints)
-CoyConUsed <- extract(Con, CoyPoints)
-CoyLicUsed <- extract(Lic, CoyPoints)
-CoyMixUsed <- extract(Mix, CoyPoints)
-CoyRocUsed <- extract(Roc, CoyPoints)
-CoyScrUsed <- extract(Scr, CoyPoints)
-CoyWatUsed <- extract(Wat, CoyPoints)
-CoyWetUsed <- extract(Wet, CoyPoints)
-CoyRugUsed <- extract(Rug, CoyPoints)
-CoyWaDUsed <- extract(WaD, CoyPoints)
-CoyLinUsed <- extract(Lin, CoyPoints)
-
-CoyRandPoints <-
-  SpatialPoints(data.frame(regPts[, 1], regPts[, 2]), proj4string = CRS(utm))
+samplePts <- rbindlist(list(regPts, coyote), 
+                       use.names = TRUE, fill = TRUE)
 
 
-CoyAntAvail <- extract(Ant, CoyRandPoints)
-CoyBroAvail <- extract(Bro, CoyRandPoints)
-CoyConAvail <- extract(Con, CoyRandPoints)
-CoyLicAvail <- extract(Lic, CoyRandPoints)
-CoyMixAvail <- extract(Mix, CoyRandPoints)
-CoyRocAvail <- extract(Roc, CoyRandPoints)
-CoyScrAvail <- extract(Scr, CoyRandPoints)
-CoyWatAvail <- extract(Wat, CoyRandPoints)
-CoyWetAvail <- extract(Wet, CoyRandPoints)
-CoyRugAvail <- extract(Rug, CoyRandPoints)
-CoyWaDAvail <- extract(WaD, CoyRandPoints)
-CoyLinAvail <- extract(Lin, CoyRandPoints)
+### Sampling ----
+# Drop columns leaving only needed
+cols <- c('id','EASTING', 'NORTHING', 'season', 'observed')
+samplePts <- samplePts[, ..cols]
 
-CNames <-
-  c(
-    "use",
-    "Ant",
-    "Bro",
-    "Con",
-    "Lic",
-    "Mix",
-    "Roc",
-    "Scr",
-    "Wat",
-    "Wet",
-    "Rug",
-    "WaD",
-    "Lin",
-    "x",
-    "y",
-    "Season"
-  )
+# Add row ID
+samplePts[, rowID := .I]
 
-CoyUsedData <-
-  data.frame(
-    1,
-    CoyAntUsed,
-    CoyBroUsed,
-    CoyConUsed,
-    CoyLicUsed,
-    CoyMixUsed,
-    CoyRocUsed,
-    CoyScrUsed,
-    CoyWatUsed,
-    CoyWetUsed,
-    CoyRugUsed,
-    CoyWaDUsed,
-    CoyLinUsed,
-    coyMRres$EASTING,
-    coyMRres$NORTHING,
-    coyMRres$season
-  )
+# Sample rasters
+lsRasters <- lapply(lsPaths, raster)
 
-colnames(CoyUsedData) <- CNames
-
-CoyAvailData <-
-  data.frame(
-    0,
-    CoyAntAvail,
-    CoyBroAvail,
-    CoyConAvail,
-    CoyLicAvail,
-    CoyMixAvail,
-    CoyRocAvail,
-    CoyScrAvail,
-    CoyWatAvail,
-    CoyWetAvail,
-    CoyRugAvail,
-    CoyWaDAvail,
-    CoyLinAvail,
-    regPts[, 1],
-    regPts[, 2],
-    "Avail"
-  )
-
-colnames(CoyAvailData) <- CNames
-
-coyoteRSF <- rbind(CoyUsedData, CoyAvailData)
+samplePts[, (lsCovers) := lapply(
+  lsRasters,
+  FUN = function(r) {
+    extract(r, matrix(c(EASTING, NORTHING), ncol = 2))
+  }
+)]
 
 
-write.csv(coyoteRSF, "output/CoyoteRSFdata.csv")
-
-coyoteRSF <- read.csv("output/CoyoteRSFdata.csv")
-
-coyoteRSF$X <- NULL
-head(coyoteRSF)
+### RSF ----
+# TODO: Remove all points with 50% NA data
 
 
+# Winter RSF
+# TODO: need ruggedness
+winterPts <- samplePts[season == "winter" | season == 'grid']
+winterPts[season == 'grid', season := "winter"]
 
+winterRSF <- glm(reformulate(lsCovers, response = 'observed'), 
+                 family = 'binomial',data = winterPts)
 
-## Remove all points with 50% NA data
-coyoteRSF$rs <- rowSums(coyoteRSF[2:10])
-coyRSF2 <- subset(coyoteRSF, rs > 0.5)
-summary(coyRSF2$rs)
+summary(winterRSF)
+vif(winterRSF)
+rsquared(winterRSF)
 
-coyRSF2[, 2] <- coyRSF2[, 2] / coyRSF2$rs
-coyRSF2[, 3] <- coyRSF2[, 3] / coyRSF2$rs
-coyRSF2[, 4] <- coyRSF2[, 4] / coyRSF2$rs
-coyRSF2[, 5] <- coyRSF2[, 5] / coyRSF2$rs
-coyRSF2[, 6] <- coyRSF2[, 6] / coyRSF2$rs
-coyRSF2[, 7] <- coyRSF2[, 7] / coyRSF2$rs
-coyRSF2[, 8] <- coyRSF2[, 8] / coyRSF2$rs
-coyRSF2[, 9] <- coyRSF2[, 9] / coyRSF2$rs
-coyRSF2[, 10] <- coyRSF2[, 10] / coyRSF2$rs
+# Pull out the coefficients, dropping the intercept
+winterCoefs <- coef(winterRSF)[-1]
 
-## Check to make sure it worked
-coyRSF2$rs2 <- rowSums(coyRSF2[2:10])
-head(coyRSF2)
+# Create the raster matching the first raster layer with the first fixed effect
+intercept <- coef(winterRSF)[1]
 
+if (all(names(winterCoefs) == names(lsRasters))) {
+  winterRaster <-
+    exp(intercept + Reduce('+', Map('*', winterCoefs, lsRasters)))
+} else {
+  stop('names dont match, check coef and rasters')
+}
 
-### Wetland is the reference
-CoySummer <- subset(coyRSF2, Season == "spring" | Season == "Avail")
-CoyWinter <- subset(coyRSF2, Season == "winter" | Season == "Avail")
+# Spring RSF
+# TODO: need ruggedness
+springwolf <- samplePts[season == "spring" | season == 'grid']
+springwolf[observed == 0, season := "spring"]
 
 RSFCoyoteSum <-
   glm(use ~ Ant + Bro + Con + Lic + Mix + Roc + Scr + WaD + Lin + Rug,
@@ -426,43 +116,54 @@ RSFCoyoteWin <-
   glm(use ~ Ant + Bro + Con + Lic + Mix + Roc + Scr + WaD + Lin + Rug,
       data = CoyWinter,
       family = 'binomial')
+springwolfRSF <- glm(reformulate(lsCovers, response = 'observed'), 
+                     family = 'binomial',data = springwolf)
 
-summary(RSFCoyoteSum)
-rsquared(RSFCoyoteSum)
+summary(springwolfRSF)
+vif(springwolfRSF)
+rsquared(springwolfRSF)
 
-summary(RSFCoyoteWin)
-rsquared(RSFCoyoteWin)
+# Pull out the coefficients, dropping the intercept
+springCoefs <- coef(springwolfRSF)[-1]
 
-SumRSF <-
-  exp(
-    coef(RSFCoyoteSum)[1] + AntR * coef(RSFCoyoteSum)[2] + BroR * coef(RSFCoyoteSum)[3] +
-      ConR * coef(RSFCoyoteSum)[4] + LicR * coef(RSFCoyoteSum)[5] +
-      MixR * coef(RSFCoyoteSum)[6] + RocR * coef(RSFCoyoteSum)[7] +
-      ScrR * coef(RSFCoyoteSum)[8] + WaDR * coef(RSFCoyoteSum)[9] +
-      LinR * coef(RSFCoyoteSum)[10] + RugR * coef(RSFCoyoteSum)[11]
-  )
+# Create the raster matching the first raster layer with the first fixed effect
+intercept <- coef(winterwolfRSF)[1]
 
-WinRSF <-
-  exp(
-    coef(RSFCoyoteWin)[1] + AntR * coef(RSFCoyoteWin)[2] + BroR * coef(RSFCoyoteWin)[3] +
-      ConR * coef(RSFCoyoteWin)[4] + LicR * coef(RSFCoyoteWin)[5] +
-      MixR * coef(RSFCoyoteWin)[6] + RocR * coef(RSFCoyoteWin)[7] +
-      ScrR * coef(RSFCoyoteWin)[8] + WaDR * coef(RSFCoyoteWin)[9] +
-      LinR * coef(RSFCoyoteWin)[10] + RugR * coef(RSFCoyoteWin)[11]
-  )
+if (all(names(winterCoefs) == names(lsRasters))) {
+  springRaster <-
+    exp(intercept + Reduce('+', Map('*', springCoefs, lsRasters)))
+} else {
+  stop('names dont match, check coef and rasters')
+}
 
-cellStats(SumRSF, max)
 
-SumRSFsc <-
-  (SumRSF - (cellStats(SumRSF, min))) / (cellStats(SumRSF, max) - cellStats(SumRSF, min))
-WinRSFsc <-
-  (WinRSF - (cellStats(WinRSF, min))) / (cellStats(WinRSF, max) - cellStats(WinRSF, min))
+### Standardize RSFs ----
+# Using feature scaling
+winterScaled <-
+  (winterRaster - (cellStats(winterRaster, min))) / (cellStats(winterRaster, max) - (cellStats(winterRaster, min)))
 
-plot(SumRSFsc)
-plot(WinRSFsc)
+springScaled <-
+  (springRaster - (cellStats(springRaster, min))) / (cellStats(springRaster, max) - (cellStats(springRaster, min)))
 
-writeRaster(SumRSFsc, "output/PredRSFNL/CoyoteSummer.tif", overwrite = T)
-writeRaster(WinRSFsc, "output/PredRSFNL/CoyoteWinter.tif", overwrite = T)
 
-saveRDS(RSFCoyoteSum, "output/PredRSFNL/CoyoteSummerRSF.RDS")
-saveRDS(RSFCoyoteWin, "output/PredRSFNL/CoyoteWinterRSF.RDS")
+### Output ----
+# Save the RSFs
+rsfs <- list('Winter' = winterScaled, 'Spring' = springScaled)
+
+lapply(
+  seq_along(rsfs),
+  FUN = function(r) {
+    writeRaster(
+      rsfs[[r]],
+      paste0('output/2-rsf/coyote/coyotersf', names(rsfs[r])),
+      format = 'GTiff',
+      overwrite = T
+    )
+  }
+)
+
+# Regular points
+saveRDS(regPts, 'output/2-rsf/coyote/coyoteRegularPoints.Rds')
+
+# Sample pts
+saveRDS(samplePts, 'output/2-rsf/coyote/coyoteSamplePoints.Rds')
