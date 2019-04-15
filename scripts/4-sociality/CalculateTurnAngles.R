3e08967bd17e7807d956cbcf93622e9cf34320b7
Alec Robitaille <robit.alec@gmail.com>
2019-04-15 15:07:43 -0700

    fin rerun angles

--- scripts/4-sociality/CalculateTurnAngles.R
+++ scripts/4-sociality/CalculateTurnAngles.R
@@ -2,8 +2,6 @@
 # Authors: Alec Robitaille
 
 
-#TODO: Grab Hance's updated social metrics from modeling script
-
 ### Packages ----
 libs <- c('data.table', 'ggplot2', 'ewc', 'SearchTrees')
 lapply(libs, require, character.only = TRUE)
@@ -20,65 +18,31 @@ DT <- readRDS(paste0('output/3-extraction/', species, 'RsfValues.Rds'))
 
 if (truelength(DT) == 0) alloc.col(DT) 
 
-
-## DECISIONS
-# turning angle is related to the t = 1 step if turning angle is 
-#   angle t = 1 and angle t = 2 
-
-
-###########
-# one <- DT[season == 'winter' & id == 862]
-# setorder(one, datetime)
 # List relevant column names
 coordCols <- c('EASTING', 'NORTHING')
 idCol <- 'id'
 datetimeCol <- 'datetime' 
 yrCol <- 'yr'
 
-calc_abs_angle(DT, coordCols, datetimeCol, by = c(idCol, yrCol, 'season'), FALSE)
+### Calculate Angles ----
+# Absolute angles
+calc_abs_angle(DT = DT,
+               coords = coordCols,
+               datetime = datetimeCol,
+               by = c('season', idCol, yrCol),
+               returnIntermediate = FALSE)
 
-one <- DT[id == sample(id, 1) & season == 'winter']
-
-x <- x+10;  
-ggplot(one[seq(x, x+10)], aes(EASTING, NORTHING)) + 
-  geom_path(aes(group = id, color = id), arrow = arrow()) + 
-  geom_text(aes(label = round(absAngle, 2)))
-  # geom_text(aes(label = round(relative, 2)))
-
-
-traj <- TrajFromCoords(
-  one,
-  xCol = 'EASTING',
-  yCol = 'NORTHING',
-  timeCol = 'datetime',
-  spatialUnits = 'm'
-)
-TrajStepLengths(traj)
-traj$angle <- c(NA, NA, TrajAngles(traj))
-
-calc_abs_angle(one, coordCols, datetimeCol, idCol, yrCol, TRUE, FALSE)
-calc_rel_angle(one, coordCols, datetimeCol, idCol, yrCol, TRUE, FALSE)
-
-
-# List relevant column names
-coordCols <- c('EASTING', 'NORTHING')
-idCol <- 'id'
-datetimeCol <- 'datetime' 
-yrCol <- 'yr'
-
-### Variables ----
-# TODO: check rleid/ http://stackoverflow.com/q/21421047/559784
-DT[, rowID := rleid(EASTING), by = c("id", "yr")]
-
-### Calculate Absolute Angle ----
-# TODO: (Ask Hance) would it be better if we had range from 0-360 instead? for dif abs angle
-calc_abs_angle(DT, coordCols, datetimeCol, idCol, yrCol, FALSE, FALSE)
-
-#TODO: check that yrcol is well handled
-calc_rel_angle(DT, coordCols, datetimeCol, idCol, yrCol, FALSE, FALSE)
+# Relative angles
+calc_rel_angle(DT = DT,
+               coords = coordCols,
+               datetime = datetimeCol,
+               by = c('season', idCol, yrCol))
 
 ### Output ----
-saveRDS(DT, paste0('output/4-angles/', species, 'Angle.Rds'))
+# Check results
+if (any(!(DT[is.na(relAngle), .N, by = .(id, yr, season)][, range(N)] == 
+          c(2, 2)))) {
+  stop('check NAs in relAngle')
+}
 
-### Figures ----
-qplot(absAngle, data = DT)
+saveRDS(DT, paste0('output/4-sociality/', species, 'Angle.Rds'))
