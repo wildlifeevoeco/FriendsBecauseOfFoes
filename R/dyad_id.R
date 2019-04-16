65efa8389b4094e0f1b5f182d9309520b724fd7e
Alec Robitaille <robit.alec@gmail.com>
2019-04-16 13:28:49 -0700

    preserve id and nn column types

--- R/dyad_id.R
+++ R/dyad_id.R
@@ -11,14 +11,20 @@
 #'
 #' @examples
 dyad_id <- function(DT, id, nn) {
+  
+  type <- typeof(DT[[id]])
+  
   g <- igraph::graph_from_data_frame(unique(na.omit(edges)[, .SD, .SDcols = c(id, nn)]),
                                      directed = FALSE)
   
   dyads <- data.table::data.table(igraph::get.edgelist(g),
                                   igraph::E(g))
-  nm <- c(paste0(id, c(1, 2)), paste0('dyad', id))
+  nm <- c(id, nn, paste0('dyad', id))
   data.table::setnames(dyads, nm)
   
+  dyads[, (c(id, nn)) := lapply(.SD, function(col) as(col, type)),
+        .SDcols = c(id, nn)]
+  return(dyads)
 }
 
 # DT
