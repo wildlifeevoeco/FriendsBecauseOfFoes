3543e565716ddae0b2fd672f8272c80d5d92e210
Alec Robitaille <robit.alec@gmail.com>
2019-04-16 08:51:39 -0700

    mv calc abs and rel to calc_angles

--- R/calc_angles.R
+++ R/calc_angles.R
@@ -1,4 +1,4 @@
-#' Calculate Absolute Angle
+#' Calculate Absolute Angles
 #'
 #' @param DT data.table
 #' @param coords character vector; coordinate columns
@@ -44,3 +44,42 @@ calc_abs_angle <- function(DT,
     return(DT)
   }
 }
+
+
+#' Calculate Relative Angles
+#'
+#' @inheritParams calc_abs_angle
+#'
+#' @return
+#' @export
+#'
+#' @examples
+calc_rel_angle <-
+  function(DT,
+           coords = NULL,
+           datetime = NULL,
+           by = NULL) {
+    # NSE
+    absAngle <- relAngle <- NULL
+    
+    if (missing(DT)) {
+      stop('must provide an input DT')
+    }
+    
+    data.table::setorderv(DT, datetime)
+    
+    if ('absAngle' %in% colnames(DT)) {
+      DT[, relAngle := absAngle -
+           data.table::shift(absAngle, 1, type = 'lag'),
+         by = by]
+      
+    } else {
+      calc_abs_angle(DT, coords, datetime, by = by)
+      
+      DT[, relAngle := absAngle -
+           data.table::shift(absAngle, 1, type = 'lag'),
+         by = by]
+    }
+    
+    return(DT)
+  }
