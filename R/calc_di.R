84f8e047c2c8ead7832151c0206d4be861904d12
Alec Robitaille <robit.alec@gmail.com>
2019-04-18 16:25:24 -0700

    use consistent suffix argument

--- R/calc_di.R
+++ R/calc_di.R
@@ -1,13 +1,21 @@
-#' Title
+#' Calculate DI
+#' 
+#' Dynamic interaction. 
 #'
 #' @param DT 
-#'
+#' @inheritParams diff_azimuth
 #' @return
 #' @export
+#' @seealso abs_angle diff_azimuth
 #'
 #' @examples
-calc_di <- function(DT) {
+calc_di <- function(DT, suffix, angle, dist) {
   # TODO: add reference to long and nelson?
+  
+  angle1 <- angle
+  angle2 <- paste0(angle, suffix)
+  
+  diff_azimuth(DT, angle1 = angle1, angle2 = angle2)
   
   
   
@@ -18,32 +26,51 @@ calc_di <- function(DT) {
 
 #' Difference in azimuth
 #'
+#' Difference in azimuth or absolute angle, calculated with \code{\link{abs_angle}}. 
+#'
 #' @inheritParams abs_angle
-#' @param angle1 azimuth angle for left individual
-#' @param angle2 azimuth angle for right individual
+#' @param suffix character to append directly to angle column name, indicating the angle of the neighbour. 
+#' @param angles character of column name with  azimuth angle.
 #'
 #' @return
+#' @seealso abs_angle
 #' @export
 #'
 #' @examples
-diff_azimuth <- function(DT, angle1, angle2) {
+diff_azimuth <- function(DT, suffix, angle) {
   # TODO: add reference to long and nelson?
   
   if (any(!(c(angle1, angle2) %in% colnames(DT)))) {
     stop('columns provided for angle1 and angle2 not found in DT')
   }
   
-  
+  # If one undefined
   DT[is.na(get(angle1)) | is.na(get(angle2)), 
       diAngle := 0]
   
+  # If both undefined
   DT[is.na(get(angle1)) & is.na(get(angle2)), 
       diAngle := 1]
   
-  
-  DT[is.na(diAngle), 
-     diAngle := cos(.SD[[1]] - .SD[[2]]), 
+  # Otherwise
+  DT[is.na(diAngle),
+     diAngle := cos(.SD[[1]] - .SD[[2]]),
      .SDcols = c(angle1, angle2)]
   
   return(DT)
+}
+
+
+
+#' Difference in distance
+#'
+#' @inheritParams diff_azimuth
+#' @param dist 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+diff_dist <- function(DT, suffix, dist) {
+  
 }
