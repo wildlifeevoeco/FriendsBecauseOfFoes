7e57b9e9bf871d47a26a175a5890f694f1da531e
Alec Robitaille <robit.alec@gmail.com>
2019-04-18 16:46:22 -0700

    add diff_dist function

--- R/calc_di.R
+++ R/calc_di.R
@@ -14,10 +14,8 @@
 #' @examples
 calc_di <- function(DT, suffix, angle, dist) {
   
-  angle1 <- angle
-  angle2 <- paste0(angle, suffix)
   
-  diff_azimuth(DT, angle1 = angle1, angle2 = angle2)
+  diff_azimuth(DT, suffix = suffix, angle = angle)
   
   
   
@@ -44,9 +42,16 @@ calc_di <- function(DT, suffix, angle, dist) {
 #' @examples
 diff_azimuth <- function(DT, suffix, angle) {
   # NSE
+  didist <- NULL
+  
+  
+  
+  
+  angle1 <- angle
+  angle2 <- paste0(angle, suffix)
   
   if (any(!(c(angle1, angle2) %in% colnames(DT)))) {
-    stop('columns provided for angle1 and angle2 not found in DT')
+    stop('angle (and angle+suffix) columns not found in DT')
   }
   
   # If one undefined
@@ -71,7 +76,7 @@ diff_azimuth <- function(DT, suffix, angle) {
 #'
 #' @inheritParams diff_azimuth
 #' @param dist 
-#' @param alpha default is 1. 
+#' @param alpha default is 1.
 #'
 #' @return
 #' 
@@ -81,5 +86,21 @@ diff_azimuth <- function(DT, suffix, angle) {
 #'
 #' @examples
 diff_dist <- function(DT, suffix, dist, alpha = 1) {
+  # NSE
+  didist <- NULL
   
+  dist1 <- dist
+  dist2 <- paste0(dist, suffix)
+  
+  if (any(!(c(dist1, dist2) %in% colnames(DT)))) {
+    stop('dist (and dist+suffix) columns not found in DT')
+  }
+  
+  DT[get(dist1) + get(dist2) == 0, diDist := 1]
+  
+  DT[is.na(diDist), 
+     diDist := 1 - ((abs(.SD[[1]] - .SD[[2]]) / .SD[[1]] + .SD[[2]]) ^ alpha),
+     .SDcols = c(dist1, dist2)]
+  
+  return(DT)
 }
