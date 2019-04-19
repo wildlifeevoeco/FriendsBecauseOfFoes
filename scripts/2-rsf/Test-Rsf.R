430a4d0844517d2116ce57b704ae69b65694d00b
Alec Robitaille <robit.alec@gmail.com>
2019-04-19 13:43:38 -0700

    rm test try command line args

--- scripts/2-rsf/Test-Rsf.R
+++
@@ -1,8 +0,0 @@
-# Test
-
-if (length(commandArgs(trailingOnly = TRUE) > 1)) {
-  species <- commandArgs(trailingOnly = TRUE)[2]
-  print(paste0('using species: ', species))
-} else {
-  species <- 'elk'
-}
